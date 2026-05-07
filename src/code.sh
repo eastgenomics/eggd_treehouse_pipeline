09bd8882aef15ef3208f324896a84cc7ede1bee8
Noepiga <noemi.piga@nhs.net>
2026-05-07 14:08:17 +0100

    correct variable for github_repo

--- src/code.sh
+++ src/code.sh
@@ -17,7 +17,7 @@ extract_pipeline() {
     # All subsequent steps assume the working directory is pipelines/
     echo ">>> Extracting Treehouse pipelines repository..."
     mkdir -p /home/dnanexus/repo_extract
-    tar -xzf "${github_repo_path}" -C repo_extract
+    tar -xzf "${github_repo}" -C repo_extract
     REPO_DIR=$(find repo_extract -mindepth 1 -maxdepth 1 -type d | head -n 1)
     if [[ -z "${REPO_DIR}" ]]; then
         echo "ERROR: failed to extract repository"
