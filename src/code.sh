972f5a9b03bee3e7c27f69a038bc2a2e882e16f7
Noepiga <noemi.piga@nhs.net>
2026-05-07 17:53:22 +0100

    correct docker imaged  code

--- src/code.sh
+++ src/code.sh
@@ -123,11 +123,13 @@ load_docker_images(){
     docker load -i /home/dnanexus/in/docker_images/0/*.tar.gz
     docker load -i /home/dnanexus/in/docker_images/1/*.tar.gz
 
+    docker images
+    
     #Replace the docker image in the Makefile
-    OLD_rnaseq='quay.io/ucsc_cgl/rnaseq-cgl-pipeline@sha256:785eee9f750ab91078d84d1ee779b6f74717eafc09e49da817af6b87619b0756'
+    OLD_rnaseq="quay.io/ucsc_cgl/rnaseq-cgl-pipeline@sha256:785eee9f750ab91078d84d1ee779b6f74717eafc09e49da817af6b87619b0756"
     NEW_rnaseq=$(docker images --format="{{.Repository}} {{.ID}}" | grep "rnaseq-cgl-pipeline" | cut -d' ' -f2)
 
-    sed -i "s#$OLD_rnaseq#$NEW_rnaseqW#g" Makefile
+    sed -i "s#$OLD_rnaseq#$NEW_rnaseq#g" Makefile
 
     OLD_umend="ucsctreehouse/bam-umend-qc@sha256:5f286d72395fcc5085a96d463ae3511554acfa4951aef7d691bba2181596c31f"
     NEW_umend=$(docker images --format="{{.Repository}} {{.ID}}" | grep "umend" | cut -d' ' -f2)
@@ -183,6 +185,7 @@ main() {
 
     dx-download-all-inputs # download inputs from json
     downgrade_docker
+    load_docker_images
     extract_pipeline
     stage_fastqs
     stage_references
